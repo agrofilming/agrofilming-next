c8d1f08469d4d39e3360df67ed03065f0935bdca
Sergey Peleh <sergey.peleh@powercode.us>
2024-05-26 15:49:11 +0300

    some fixes

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,4 +1,9 @@
 import { BlogPage } from '@/containers/BlogPage';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Блог - AgroFilmProduction',
+};
 
 export default function Blog() {
   return <BlogPage />;
